e8d23715a87fa90028b5256b6098f1a96fcc1fe8
DokutahShikikan <dimka_din_4e@mail.ru>
2026-04-29 16:12:14 +0600

    Подготовил проект к запуску в Docker и очистил репозиторий

--- diploma-app/storage/framework/views/5bdee2b460c0a312e0661c5404655541.php
+++
@@ -1,5 +0,0 @@
-<div <?php echo e($attributes->class('flex flex-wrap gap-4')); ?>>
-    <?php echo e($slot); ?>
-
-</div>
-<?php /**PATH C:\Users\minho\Desktop\Курсча_преддиплом\diploma-app\resources\views/components/form-actions.blade.php ENDPATH**/ ?>
